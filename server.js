52d54a517aa55a2a55f00d09c78dbda3f8a847cb
Illgner Alain <illgneralain@gmail.com>
2021-02-09 17:06:55 -0500

    made changes to the copied and pasted activity to fit hw requirements

--- server.js
+++ server.js
@@ -14,3 +14,52 @@ var connection = mysql.createConnection({
   password: "password123",
   database: "employee-tracker"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+  });
+  
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add employee",
+          "Add role",
+          "Add department",
+          "View all employees",
+          "Remove employee",
+          "View all roles",
+          "Update Employee Role",
+          "View all departments",
+          "Exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+  
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
+  
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
+  
+        case "Search for a specific song":
+          songSearch();
+          break;
+  
+        case "Find artists with a top song and top album in the same year":
+          songAndAlbumSearch();
+          break;
+        }
+      });
+  }
+  
